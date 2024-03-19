935d6efac06c67a12dfdeb26fe93f66096c764f3
Eeritvan <eetu.ritvanen@elisanet.fi>
2024-03-19 13:53:40 +0200

    created basic login system

---
+++ routes.py
@@ -0,0 +1,38 @@
+from app import app
+from flask import redirect, render_template, request
+import users
+
+@app.route("/")
+def frontpage():
+    return render_template("frontpage.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("register.html")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("register.html")
