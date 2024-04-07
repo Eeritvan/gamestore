9bfb3fab5d1e9cf0245737d90c4143615a29ba2f
Eeritvan <eetu.ritvanen@elisanet.fi>
2024-04-07 18:10:52 +0300

    Added application to fly.io

--- db.py
+++ db.py
@@ -2,5 +2,11 @@ from os import getenv
 from flask_sqlalchemy import SQLAlchemy
 from app import app
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+URL = getenv("DATABASE_URL")
+
+if getenv("on_fly"):
+    URL = URL.replace("://", "ql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = URL
+
 db = SQLAlchemy(app)
